990ff11db6a8f910024ea480892803c4f5c94408
Martin Fenner <mfenner@plos.org>
2015-01-15 23:20:46 +0100

    look for unique DOIs

--- random_dois.R
+++ random_dois.R
@@ -2,4 +2,5 @@ library('rcrossref')
 
 #dois <- cr_r(15000, filter=c(from_pub_date='2013-01-01', until_pub_date='2013-12-31'))
 #csv <- as.data.frame(dois)
-write.csv(csv, "dois.csv", row.names=FALSE, quote=FALSE, fileEncoding="utf-8")
+csv <- unique(csv)
+#write.csv(csv, "dois.csv", row.names=FALSE, quote=FALSE, fileEncoding="utf-8")

f4b6a6c1eb326908d7397e240895296427646c59
Martin Fenner <mfenner@plos.org>
2015-01-15 23:40:01 +0100

    restrict to type journal-article

--- random_dois.R
+++ random_dois.R
@@ -1,6 +1,5 @@
 library('rcrossref')
 
-#dois <- cr_r(15000, filter=c(from_pub_date='2013-01-01', until_pub_date='2013-12-31'))
-#csv <- as.data.frame(dois)
-csv <- unique(csv)
-#write.csv(csv, "dois.csv", row.names=FALSE, quote=FALSE, fileEncoding="utf-8")
+dois <- cr_r(15000, filter=c(from_pub_date='2013-01-01', until_pub_date='2013-12-31', type='journal-article'))
+csv <- unique(as.data.frame(dois))
+write.csv(csv, "random_crossref_dois_2013.csv", row.names=FALSE, quote=FALSE, fileEncoding="utf-8")

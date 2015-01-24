0e60dfc1affc12a22cbc26feed466e2da924fc96
Martin Fenner <mfenner@plos.org>
2015-01-24 17:35:42 +0100

    added more attributes to random set of DOIs

---
+++ merge_data.R
@@ -0,0 +1,12 @@
+data <- read.csv("random_crossref_dois_2013_1.csv", stringsAsFactors=FALSE, sep=",", header=TRUE)
+
+for (i in 2:20) {
+  file <- paste("random_crossref_dois", "_", "2013", "_", i, ".csv", sep = "")
+  temp <- read.csv(file, stringsAsFactors=FALSE, sep=",", header=TRUE)
+  data <- rbind(data, temp)
+}
+
+data <- unique(data)
+data <- data[sample(1:nrow(data), 15000,replace=FALSE),]
+
+write.csv(data, "random_crossref_dois_2013.csv", row.names=FALSE, fileEncoding="utf-8")
--- random_dois.R
+++ random_dois.R
@@ -1,5 +1,24 @@
 library('rcrossref')
+library('stringr')
 
-dois <- cr_r(15000, filter=c(from_pub_date='2013-01-01', until_pub_date='2013-12-31', type='journal-article'))
-csv <- unique(as.data.frame(dois))
-write.csv(csv, "random_crossref_dois_2013.csv", row.names=FALSE, quote=FALSE, fileEncoding="utf-8")
+splitColumn <- function(df, colname) {
+  string <- df[colname]
+  str_split(iconv(string, "latin1", "UTF-8"), ",")[[1]][1]
+}     
+
+for (i in 1:20) {
+  result <- cr_works(sample = 1000, filter=c(from_pub_date='2013-01-01', until_pub_date='2013-12-31', type='journal-article'))
+  data <- unique(as.data.frame(result$data))
+  data$doi <- data$DOI
+  data$publication_date <- data$issued
+  data$title <- iconv(str_trim(data$title), "latin1", "UTF-8")
+  # only fetch first journal title
+  data$journal <- apply(data, 1, splitColumn, colname = "container.title")
+  # only fetch first ISSN
+  data$issn <- apply(data, 1, splitColumn, colname = "ISSN")
+  # extract CrossRef member id from URL
+  data$member_id <- str_extract(data$member, "\\d+")
+  data <- subset(data, select=c("doi", "publication_date", "title", "journal", "issn", "publisher", "member_id"))
+  file <- paste("random_crossref_dois", "_", "2013", "_", i, ".csv", sep = "")
+  write.csv(data, file, row.names=FALSE, fileEncoding="UTF-8")
+}
